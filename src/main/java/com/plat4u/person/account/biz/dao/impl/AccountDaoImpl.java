a1503cde633adf612c3253dab5d46a5b65b8493a
design-library <net.shinya@gmail.com>
2017-04-20 00:27:12 +0900

    初回登録

---
+++ src/main/java/com/plat4u/person/account/biz/dao/impl/AccountDaoImpl.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2017 plat4u.com
+ * 
+ * This file is part of person elements.
+ *
+ *  plat4u.com is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  plat4u.com is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with plat4u.com.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.plat4u.person.account.biz.dao.impl;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.springframework.stereotype.Repository;
+
+import com.plat4u.person.account.biz.dao.AccountDao;
+import com.plat4u.person.account.biz.entity.AccountEntity;
+
+/**
+ * AccountDaoImpl
+ *
+ * @author plat4u.com
+ * @version 1.0
+ */
+@Repository
+public class AccountDaoImpl implements AccountDao {
+	
+	@PersistenceContext
+    EntityManager entityManager;
+	
+	/* (”ñ Javadoc)
+	 * @see com.plat4u.person.account.biz.dao.AccountDao#findOne(com.plat4u.person.account.biz.entity.AccountEntity)
+	 */
+	public AccountEntity findOne(AccountEntity entity) {
+		
+		Query query = entityManager.createNamedQuery("Account.findOne", AccountEntity.class);
+		query.setParameter("id", entity.getId());
+		query.setParameter("password", entity.getPassword());
+		AccountEntity accountEntityRtn = (AccountEntity)query.getSingleResult();
+		
+		return accountEntityRtn;
+	}
+
+}
---
+++ src/main/java/com/plat4u/person/account/biz/service/impl/AccountServiceImpl.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright 2017 plat4u.com
+ * 
+ * This file is part of person elements.
+ *
+ *  plat4u.com is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  plat4u.com is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with plat4u.com.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.plat4u.person.account.biz.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.plat4u.person.account.biz.dao.AccountDao;
+import com.plat4u.person.account.biz.domain.Account;
+import com.plat4u.person.account.biz.entity.AccountEntity;
+import com.plat4u.person.account.biz.service.AccountService;
+import com.plat4u.person.exception.AuthenticationException;
+
+/**
+ * AccountServiceImpl
+ *
+ * @author plat4u.com
+ * @version 1.0
+ */
+@Service
+public class AccountServiceImpl implements AccountService {
+
+	@Autowired
+	private AccountDao accountDao;
+	
+	/* (非 Javadoc)
+	 * @see com.plat4u.person.account.biz.service.AccountService#authenticate(com.plat4u.person.account.biz.domain.Account)
+	 */
+	public Account authenticate(Account account) throws AuthenticationException {
+		
+		// create AccountEntity.
+		AccountEntity accountEntity = new AccountEntity();
+		accountEntity.setId(account.id().id());
+		accountEntity.setPassword(account.password().stretch());
+		
+		// execute.
+		AccountEntity accountEntityRtn = accountDao.findOne(accountEntity);
+		
+		// create Account
+		Account accountRtn = new Account(accountEntityRtn.getId(), accountEntityRtn.getPassword());
+		
+		return accountRtn;
+	}
+
+	/* (非 Javadoc)
+	 * @see com.plat4u.person.account.biz.service.AccountService#create(com.plat4u.person.account.biz.domain.Account)
+	 */
+	public Account create(Account account) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	/* (非 Javadoc)
+	 * @see com.plat4u.person.account.biz.service.AccountService#updatePassword(com.plat4u.person.account.biz.domain.Account)
+	 */
+	public Account updatePassword(Account account) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
