bc761fd475bbbf73c21507d6931fb166b9567b8c
design-library <net.shinya@gmail.com>
2017-07-03 07:15:54 +0900

    カレントディレクトリの追加

--- gate_scheama.sh
+++ gate_scheama.sh
@@ -4,7 +4,7 @@ echo "MYSQL LOGIN & CREATE TEST DATABASE/TABLES."
 
 mysql -u root -paws.mysql@8944 <<EOF
 
-source /src/test/resources/META-INF/use_to_utest.sql
+source ./src/test/resources/META-INF/use_to_utest.sql
 
 EOF
 
